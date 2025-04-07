8d0e0a08a4e9f7775cbee671a42ef972d6f19f72
Luke Eades <lukeeadeswork@gmail.com>
2025-04-07 11:23:39 -0700

    added readme for phonebook backend

---
+++ part3/phonebook/README.md
@@ -0,0 +1,1 @@
+<https://phonebook-backend-8w4k.onrender.com>
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -88,7 +88,7 @@ app.post("/api/persons", (request, response) => {
     response.status(201).json(person).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`live on port ${PORT}`);
