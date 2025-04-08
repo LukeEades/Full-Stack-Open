f2fb6cea3009f96502e6f21289b8cee578198c23
Luke Eades <lukeeadeswork@gmail.com>
2025-04-08 11:42:14 -0700

    factored out config

---
+++ part4/bloglist/app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const { MONGODB_URL } = require("./utils/config.js")
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(MONGODB_URL)
+    .then(() => {
+        console.log("successfully connected to db")
+    })
+    .catch(err => {
+        console.log("unable to connect", err)
+    })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
